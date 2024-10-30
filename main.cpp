f9c65f7535cf4f7c9f5ce016e07010db26252f70
giannis <karelasgiannis@gmail.com>
2024-10-30 21:22:29 +0200

    make the code with switch statement

--- main.cpp
+++ main.cpp
@@ -9,20 +9,28 @@ int main() {
         cout << "Please enter a number between [1,10]: ";
         cin >> x;
     } while (x>10 || x<1);
-                                    // for example: 5.39
-    x=x*10;                         // 53.9
-    int y= x;                       // 53
-    float result=x-y;               // 53.9-53 =0.9
 
-    if(result>=0.5) {
-        int k=(int)x +1;                    // 54
-        float FinalResult=(float)k/10;      // 5.4
-        cout<< "The rounded number is: "<<FinalResult<<endl;
-    }
-    else {                                  // if x=5.33 & x=x*10=53.3 & result = 0.3
-        int k=(int)x;                       // k=53
-        float FinalResult=(float)k/10;      // FinalResult = 5.3
-        cout<< "The rounded number is: "<<FinalResult<<endl;
+    x=x*10;
+    int y= x;
+    float result=x-y;
+
+    int k;
+    float FinalResult;
+
+    float prechoice = result*10;
+    int choice= (int) prechoice;
+
+    switch(choice) {
+        case 0: case 1: case 2: case 3: case 4:
+            k=(int)x;
+            FinalResult=(float)k/10;
+            cout<< "The rounded number is: "<<FinalResult<<endl;
+            break;
+        case 5:case 6:case 7:case 8:case 9:
+            k=(int)x +1;
+            FinalResult=(float)k/10;
+            cout<< "The rounded number is: "<<FinalResult<<endl;
+            break;
     }
     return 0;
 }
