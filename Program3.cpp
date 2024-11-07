e7b06cb0b4fe7a592cb8fd520f3ec941933ababb
johnk555 <karelasgiannis@gmail.com>
2024-11-07 15:40:33 +0200

    Update Program3.cpp

--- Program3.cpp
+++ Program3.cpp
@@ -1,39 +1,21 @@
 #include <iostream>
-
-#include <string>
-
+#include <cmath>
 using namespace std;
-
 
 int main()
 {
-    
-string mystring;
-    
-    
-mystring = "This is a string" ;
-    
-    
-cout << mystring << endl;
-    
-    
-string onoma,eponimo;
-    
-    
-cout << "Dose onoma: ";
-    
-cin >> onoma;
-    
-    
-cout << "Dose eponimo: ";
-    
-cin >> eponimo;
-    
-    
-cout<< eponimo << " , " << onoma;
-    
-    
+int bit;
+int arr[5];
+int result=0;
+ 
+for(int i=0; i<5; i++){      
+cout<<"Dwse to "<<i+1<<"-o bit: ";
+        
+cin>>arr[i];
+        
+result += arr[i]*pow(2,i);    
+}
+cout<<result;
+ 
 return 0;
-
-
 }
