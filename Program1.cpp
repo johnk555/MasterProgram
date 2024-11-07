954c0f0db0cd04178399f0de375a4b264525ac57
johnk555 <karelasgiannis@gmail.com>
2024-11-07 15:37:12 +0200

    Create Program1.cpp

---
+++ Program1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    
+int x=5,y=2;
+    
+cout << x+y << "\n";
+    
+cout << x+y << endl;
+    
+   
+cin >> x;
+   
+cout << x+y;
+    
+   
+return 0;
+
+
+}
