4bfdabbbbeb9cd3f46c077ca2079c22866205e3e
johnk555 <karelasgiannis@gmail.com>
2024-11-07 15:38:14 +0200

    Create Program4.cpp

---
+++ Program4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+#define PI 3.14159
+#define NEWLINE '\n'
+
+int main ()
+{
+  int x=6, y=4;
+  
+  float apotelesma;
+  
+  apotelesma =  (float)x/y;
+  
+  cout << "apotelesma = " << apotelesma << endl;
+  
+  return 0;
+
+}
