bc6a241ce364afa2067b806cf29f107a91a2a970
johnk555 <karelasgiannis@gmail.com>
2024-11-07 15:39:50 +0200

    Update Program2.cpp

--- Program2.cpp
+++ Program2.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 
+#include <cmath>
 using namespace std;
+
 
 
 int main()
 {
+  
+  
+int amount;
+ 
+  
+   
+do{
     
-int x=5,y=2;
+cout<<"Dwse to poso twn xrimatwn: ";
+    
+cin>>amount;
+    
+}while(amount<=0 || (amount%50!=0 && amount%20!=0 && amount%10!=0));
+
+
+
     
     
-cout << "Dose akeraio: ";
+int num_50 = (float) amount/50;
     
-cin >> x;
+int num_20 = (float) (amount- 50*num_50)/20;
     
+int num_10 = (float) (amount - 50*num_50 - 20*num_20)/10;
+
+
+
+
     
-cout << "To apotelesma x=" << x <<" kai y=" << y << " einai " ;
+cout<<"50eura: "<<num_50<<endl<<"20eura: "<<num_20<<endl<<"10eura: "<<num_10;
+
+
     
-cout<< x+y << endl;
-    
-    
+
+
 return 0;
-
-
 }
