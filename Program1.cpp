e77ff525af8282f0fadb15eca9cc524ea65bc2f1
johnk555 <karelasgiannis@gmail.com>
2024-11-07 15:39:23 +0200

    Update Program1.cpp

--- Program1.cpp
+++ Program1.cpp
@@ -1,24 +1,56 @@
 #include <iostream>
 
+#include <cmath>
+
 using namespace std;
 
 
 int main()
 {
     
-int x=5,y=2;
+float a,b,c;
     
-cout << x+y << "\n";
+cout<<"Dwse tous syntelestes ax2 + bx +c = 0"<<endl;
     
-cout << x+y << endl;
+cout<<"Dwse a: ";
     
-   
-cin >> x;
-   
-cout << x+y;
+cin >> a;
     
-   
-return 0;
+cout<<"Dwse b: ";
+    
+cin >> b;
+    
+cout<<"Dwse c: ";
+    
+cin >> c;
+    
+    
+//D gia diakrinousa
+    
+float D = b*b - 4*a*c;
+    
+    
+if(D >= 0){
+    
+double result1 = (-b-sqrt((double)D))/(2*a);
+    
+double result2 = (-b+sqrt((double)D))/(2*a);
+    
+    
+cout<<"Oi lyseis tis eksiswsis einai: "<<result1 <<" , " <<result2 <<endl;
+    
+}
+    
+else if(D == 0)
+        
+cout<<"H lysi einai: "<<result1;
+    
+else
+        
+cout<<"H eksiswsi den exei pragmatikes rizes!";
 
+    
+
+return 0;
 
 }
