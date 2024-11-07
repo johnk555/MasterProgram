661f38aa36c14abe5dc1acb2abc56573f60f0d69
johnk555 <karelasgiannis@gmail.com>
2024-11-07 15:37:55 +0200

    Create Program3.cpp

---
+++ Program3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    
+string mystring;
+    
+    
+mystring = "This is a string" ;
+    
+    
+cout << mystring << endl;
+    
+    
+string onoma,eponimo;
+    
+    
+cout << "Dose onoma: ";
+    
+cin >> onoma;
+    
+    
+cout << "Dose eponimo: ";
+    
+cin >> eponimo;
+    
+    
+cout<< eponimo << " , " << onoma;
+    
+    
+return 0;
+
+
+}
